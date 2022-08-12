38478108f8f183c82ea435e78772179a9698e13a
Luan Teixeira <luanlts@outlook.com>
2022-08-12 14:52:31 -0300

    feat(apostila03): retomar do bloco de codigo 2.7.3

--- src/index.js
+++ src/index.js
@@ -3,8 +3,51 @@ import ReactDOM  from 'react-dom'
 
 const App = () => {
     return ( // devolve a representacao grafica do componente app
-        <div>
-            meu primeiro componente React JS
+        <div 
+            style={{
+                margin: 'auto',
+                width: 768,
+                backgroundColor: '#eee',
+                padding: 12,
+                borderRadius: 8
+            }}
+        >
+        <label
+            htmlFor='nome'
+            style={{
+                display: 'block',
+                marginBotton: 4
+            }}
+        >
+            Nome:
+        </label>
+        <input
+            type="text"
+            id='nome' 
+            style={{
+                paddingTop: 8,
+                paddingBottom: 8,
+                borderStyle: 'hidden',
+                outline: 'none',
+                width: '100%',
+                borderRadius: 8,
+            }}    
+        />
+            <button
+                style={{
+                    marginTop: 12,
+                    paddingTop: 8,
+                    paddingBottom: 8,
+                    backgroundColor: 'blueviolet',
+                    color: 'white',
+                    border: 'none',
+                    width: '100%',
+                    borderRadius: 8,
+                    cursor: "pointer"
+                }}
+            >
+                Enviar
+            </button>
         </div>
     )
 }
